5393a3635433625c6874bdbf11617d812c3ecbc7
아이온 <ion@aion-ui-MacBook-Pro.local>
2019-02-07 19:28:08 +0900

    Initial commit
    Create Project - DataIo

---
+++ src/main/java/com/test/dataio/DataioApplication.java
@@ -0,0 +1,14 @@
+package com.test.dataio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataioApplication.class, args);
+	}
+
+}
+
